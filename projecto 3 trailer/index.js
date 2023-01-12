c6f1a2d21e22b5a37a854b9a63633aab39204263
miguelfhg <122413712+miguelfhg@users.noreply.github.com>
2023-01-12 17:04:12 +0000

    Add files via upload

---
+++ projecto 3 trailer/index.js
@@ -0,0 +1,12 @@
+const btneEl = document.querySelector(".btn");
+const closeIconEl = document.querySelector(".close-icon");
+const trailerContainerEl = document.querySelector(".trailer-container");
+const videoEl = document.querySelector("video");
+btneEl.addEventListener("click",() =>{ trailerContainerEl.classList.remove ("active");
+});
+
+closeIconEl.addEventListener("click",() =>{ trailerContainerEl.classList.add ("active");
+videoEl.pause();
+videoEl.currentTime = 0;
+
+})
